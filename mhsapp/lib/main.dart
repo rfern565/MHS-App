3f5faab75edb48daba9edb637d3713b290f2071c
Ryan Fernandez <120501977+rfern565@users.noreply.github.com>
2023-05-16 00:26:16 -0400

    Added FAQ Button for homepage

--- mhsapp/lib/main.dart
+++ mhsapp/lib/main.dart
@@ -15,6 +15,8 @@ class MyApp extends StatelessWidget {
         '/mySchedule': (context) => MySchedulePage(),
         '/myClubs': (context) => MyClubsPage(),
         '/browseClubs': (context) => BrowseClubsPage(),
+        '/questions': (context) => questionsPage(),
+
       },
     );
   }
@@ -24,105 +26,125 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: Text(
-                'Hello, Mia!',
-                style: TextStyle(
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 32),
-              child: Text(
-                'What are you looking for?',
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/mySchedule');
-                },
-                child: Text(
-                  ' My Schedule ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
+      body: Stack(
+        children: [
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Hello, Mia!',
+                    style: TextStyle(
+                      fontSize: 45,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(200, 41),
-                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/myClubs');
-                },
-                child: Text(
-                  '    My Clubs    ',
-                  style: TextStyle(fontSize: 20),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
+                SizedBox(height: 16),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 32),
+                  child: Text(
+                    'What are you looking for?',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(200, 41),
-                  ),
                 ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8),
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/browseClubs');
-                },
-                child: Text(
-                  'Browse Clubs',
-                  style: TextStyle(fontSize: 20),
-                ),
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(50.0),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/mySchedule');
+                    },
+                    child: Text(
+                      ' My Schedule ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                      ),
+                      minimumSize: MaterialStateProperty.all<Size>(
+                        Size(200, 41),
+                      ),
                     ),
                   ),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(200, 41),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/myClubs');
+                    },
+                    child: Text(
+                      '    My Clubs    ',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                      ),
+                      minimumSize: MaterialStateProperty.all<Size>(
+                        Size(200, 41),
+                      ),
+                    ),
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/browseClubs');
+                    },
+                    child: Text(
+                      'Browse Clubs',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(Colors.black),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                      ),
+                      minimumSize: MaterialStateProperty.all<Size>(
+                        Size(200, 41),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/questions');
+              },
+              backgroundColor: Colors.orange,
+              child: Icon(
+                Icons.question_answer,
+                color: Colors.white,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
 }
+
 
 
 
@@ -172,6 +194,18 @@ class BrowseClubsPage extends StatelessWidget {
   }
 }
 
-
+class questionsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Questions'),
+      ),
+      body: Center(
+        child: Text('Questions Page'),
+      ),
+    );
+  }
+}
 
 
